a90d63d4e8873fe3b90ceef925e0e5221d3914a4
Cameron Hunter <chunter@twitter.com>
2016-03-27 23:34:15 -0700

    Support building a production version of the library

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,17 @@
 import babel from 'rollup-plugin-babel';
 import pegjs from 'rollup-plugin-pegjs';
+import uglify from 'rollup-plugin-uglify';
+
+const isProduction = process.env.NODE_ENV === 'production';
+const nonNull = (array) => array.filter(item => !!item);
 
 export default {
   entry: 'src/twitter-lang.pegjs',
-  dest: 'build/twitter-lang.js',
+  dest: `build/twitter-lang${isProduction ? '.min' : ''}.js`,
   format: 'cjs',
-  plugins: [
+  plugins: nonNull([
     pegjs(),
-    babel({ babelrc: false, presets: ['es2015-rollup', 'stage-1'] })
-  ]
+    babel({ babelrc: false, presets: ['es2015-rollup', 'stage-1'] }),
+    isProduction ? uglify() : null
+  ])
 }
