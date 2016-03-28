569f9596042b94b62d0960c0989e11fcef38f6af
Cameron Hunter <chunter@twitter.com>
2016-03-28 00:29:23 -0700

    Add partial URL support

---
+++ test/url-test.js
@@ -0,0 +1,35 @@
+import test from 'ava';
+import { parse } from '..';
+
+test('Single URL', t => {
+  const actual = parse('http://twitter.com');
+  const expected = {
+    text: 'http://twitter.com',
+    entities: {
+      hashtags: [],
+      symbols: [],
+      urls: [{ url: 'http://twitter.com', indices: [0, 18] }],
+      user_mentions: []
+    }
+  };
+
+  t.same(actual, expected);
+});
+
+test('Multiple URLs', t => {
+  const actual = parse('http://twitter.com/ https://google.com');
+  const expected = {
+    text: 'http://twitter.com/ https://google.com',
+    entities: {
+      hashtags: [],
+      symbols: [],
+      urls: [
+        { url: 'http://twitter.com/', indices: [0, 19] },
+        { url: 'https://google.com', indices: [20, 38] }
+      ],
+      user_mentions: []
+    }
+  };
+
+  t.same(actual, expected);
+});
