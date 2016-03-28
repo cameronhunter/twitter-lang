a39ceb5990eb48c01d28123ee01841e5adf6f62f
Cameron Hunter <chunter@twitter.com>
2016-03-27 18:23:42 -0700

    Improve space tests

--- test/space-test.js
+++ test/space-test.js
@@ -2,27 +2,15 @@ import test from 'ava';
 import { parse } from '..';
 
 test('\\u0020', t => {
-  const actual = parse('hello\u0020world');
+  const actual = parse('$twtr\u0020$twtr');
   const expected = {
-    text: 'hello\u0020world',
+    text: '$twtr\u0020$twtr',
     entities: {
       hashtags: [],
-      symbols: [],
-      urls: [],
-      user_mentions: []
-    }
-  };
-
-  t.same(actual, expected);
-});
-
-test('\\u0085', t => {
-  const actual = parse('hello\u0085world');
-  const expected = {
-    text: 'hello\u0085world',
-    entities: {
-      hashtags: [],
-      symbols: [],
+      symbols: [
+        { text: 'twtr', indices: [0, 5] },
+        { text: 'twtr', indices: [6, 11] }
+      ],
       urls: [],
       user_mentions: []
     }
@@ -32,12 +20,15 @@ test('\\u0085', t => {
 });
 
 test('\\u2001', t => {
-  const actual = parse('hello\u2001world');
+  const actual = parse('$twtr\u2001$twtr');
   const expected = {
-    text: 'hello\u2001world',
+    text: '$twtr\u2001$twtr',
     entities: {
       hashtags: [],
-      symbols: [],
+      symbols: [
+        { text: 'twtr', indices: [0, 5] },
+        { text: 'twtr', indices: [6, 11] }
+      ],
       urls: [],
       user_mentions: []
     }
